ff2ebb16d5b7b2ea0386fd851abebcb1f0bff4be
BSipos-RKF <BSipos@rkf-eng.com>
2021-03-12 20:42:06 +0000

    Deploying to gh-pages from @ BSipos-RKF/dtn-neighbor-msg@4ce3509ffc8c8d6ed4935a84f8eda178930aea79 🚀

--- CMakeFiles/cborerrors.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cborerrors"
-  "cborerrors.txt"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cborerrors.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
